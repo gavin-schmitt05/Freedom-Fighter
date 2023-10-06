9892109f7e1a009113f334388e278f55756fcd1c
Macario <mspina5960@student.pps.net>
2023-10-06 10:28:54 -0700

    run & idle & player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,13 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 5f;
     private float direction = 0f;
+    public float jumpSpeed = 8f;
     private Rigidbody2D player;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +25,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
         direction = Input.GetAxis("Horizontal");
 
         if (direction > 0f)
@@ -34,6 +42,20 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             player.velocity = new Vector2(0, player.velocity.y);
+        }
+
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            player.velocity = new Vector2(player.velocity.x, jumpSpeed);
+        }
+
+        if (direction > 0)
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (direction > 0)
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
         }
     }
 }
