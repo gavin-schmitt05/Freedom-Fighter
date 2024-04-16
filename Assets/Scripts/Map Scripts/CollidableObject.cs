551e69dbca7ace672fc8b63e3c540e9b27c5c75e
Mac <mspina5960@student.pps.net>
2024-04-16 11:39:51 -0700

    interaction, timer, RPG sprite

--- Assets/Scripts/Map Scripts/CollidableObject.cs
+++ Assets/Scripts/Map Scripts/CollidableObject.cs
@@ -4,32 +4,27 @@ using UnityEngine;
 
 public class CollidableObject : MonoBehaviour
 {
-
     private Collider2D z_Collider;
     [SerializeField]
     private ContactFilter2D z_Filter;
-    private List<Collider2D> z_collidedObjects = new List<Collider2D>(1);
+    private List<Collider2D> z_CollidedObjects = new List<Collider2D>(1);
 
-    // Start is called before the first frame update
     protected virtual void Start()
     {
         z_Collider = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
-   protected virtual void Update()
+    protected virtual void Update()
     {
-        z_Collider.OverlapCollider(z_Filter, z_collidedObjects);
-        foreach(var o in z_collidedObjects)
+        z_Collider.OverlapCollider(z_Filter, z_CollidedObjects);
+        foreach(var o in z_CollidedObjects)
         {
-           Debug.Log("Collided with " + o.name);
+            OnCollided(o.gameObject);
         }
     }
-
 
     protected virtual void OnCollided(GameObject collidedObject)
     {
         Debug.Log("Collided with " + collidedObject.name);
-
     }
 }
--- Assets/Scripts/Map Scripts/InteractableObject.cs
+++ Assets/Scripts/Map Scripts/InteractableObject.cs
@@ -5,24 +5,36 @@ using UnityEngine;
 public class InteractableObject : CollidableObject
 {
     private bool z_Interacted = false;
-  protected override void OnCollided(GameObject collidedObject)
-  {
-    if(Input.GetKeyDown("e"))
-    {
-        Debug.Log("e was pressed");
-        OnInteract();
-        
-   }
-  }
 
-  protected virtual void OnInteract()
-  {
+    public GameObject EnemySpawner;
+
+  //  public Timer time;
+
+    public GameObject Timer;
+
+
+
+    protected override void OnCollided(GameObject collidedObject)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            OnInteract();
+        }
+    }
+
+    protected virtual void OnInteract()
+    {
         if (!z_Interacted)
         {
-           z_Interacted = true; 
-           Debug.Log("INTERACT WITH" + name);
-        }
+            z_Interacted = true;
+            Debug.Log("INTERACT WITH " + name);
+            EnemySpawner.SetActive(true);
+            Timer.SetActive(true);
 
-        
-  }
+            
+        }        
+    }
+
+
+
 }
