8bc6122842922eacc57f704b5cd0d67ec3b9675c
Gavin Schmitt <gavinschmitt05@gmail.com>
2024-05-23 16:16:42 -0700

    Airdrop works now, lets goooooo

--- Assets/Scripts/Player/Mech/MechFlare.cs
+++ Assets/Scripts/Player/Mech/MechFlare.cs
@@ -1,23 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class MechFlare : MonoBehaviour
 {
     public Rigidbody2D rb;
     public GameObject Mech;
-    public Transform airdropPoint;
+    private GameObject airdropPoint;
     private float Speed = 4f;
     
     // Start is called before the first frame update
     void Start()
     {
+        airdropPoint = GameObject.Find("Airdrop Point");
         rb.velocity = this.transform.right * Speed;
         StartCoroutine(MechDropper());
     }
     IEnumerator MechDropper()
     {
-        Destroy(this, 3);
-        yield return new WaitForSeconds(4f);
-        airdropPoint.position = new Vector3(this.transform.position.x, 75, 0);
-        Instantiate(Mech, airdropPoint.position, airdropPoint.rotation);
+        Destroy(this.gameObject, 5);
+        yield return new WaitForSeconds(4);
+        airdropPoint.transform.position = new Vector3(this.gameObject.transform.position.x, 75, 0);
+        Instantiate(Mech, airdropPoint.transform.position, airdropPoint.transform.rotation);
     }
 
 }
